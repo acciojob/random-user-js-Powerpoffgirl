72091cda8962c897d249ad9e330b3fd07f2d5214
Jyoti Singh <80877414+Powerpoffgirl@users.noreply.github.com>
2023-01-09 12:35:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,42 +8,36 @@ let fullName = "";
 let age = "";
 let email = "";
 let phone = "";
+let user = {};
+
+fetch("https://randomuser.me/api/")
+  .then((response) => response.json())
+  .then((data) => {
+    user = data.results[0];
+    fullName = data.results[0].name.first + " " + data.results[0].name.last;
+    console.log(fullName);
+    console.log(user);
+    fullNameId.innerText = fullName;
+  });
 
 ageId.addEventListener("click", showAge);
 emailId.addEventListener("click", showEmail);
 phoneId.addEventListener("click", showPhone);
 
-fetch("https://randomuser.me/api/")
-  .then((response) => response.json())
-  .then((data) => {
-    fullName = data.results[0].name.first + " " + data.results[0].name.last;
-    console.log(fullName);
-    console.log(data.results);
-    fullNameId.innerText = fullName;
-  });
-
 function showAge() {
-  age = data.results[0].dob.age;
+  age = user.dob.age;
   console.log(age);
   result.innerText = age;
 }
 
 function showEmail() {
-  fetch("https://randomuser.me/api/")
-    .then((response) => response.json())
-    .then((data) => {
-      email = data.results[0].email;
-      console.log(email);
-      result.innerText = email;
-    });
+  email = user.email;
+  console.log(email);
+  result.innerText = email;
 }
 
 function showPhone() {
-  fetch("https://randomuser.me/api/")
-    .then((response) => response.json())
-    .then((data) => {
-      phone = data.results[0].phone;
-      console.log(phone);
-      result.innerText = phone;
-    });
+  phone = user.phone;
+  console.log(phone);
+  result.innerText = phone;
 }
